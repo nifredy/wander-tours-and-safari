9aa31f8a105858fff62b413c462e2f16052dd8cc
nifredy <fredydominic0@yahoo.com>
2021-10-03 01:23:39 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,103 @@
+/*window.addEventListener("scroll", function () {
+  var header = document.getElementById("header");
+  header.classList.toggle("sticky", window.scrollY);
+});
+
+ showing the menu */
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
+
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+
+/* remove menu mobileeeee */
+
+const navLink = document.querySelectorAll(".nav-link");
+
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show-menu");
+}
+
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+/* scrollllll activeeee */
+
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav-menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav-menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
+
+//scroll uppppppppp staffff
+
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+
+  if (this.scrollY >= 200) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollUp);
+
+// background header sticky
+
+function scrollHeader() {
+  const nav = document.getElementById("header");
+
+  if (this.scrollY >= 5) nav.classList.add("sticky");
+  else nav.classList.remove("sticky");
+}
+window.addEventListener("scroll", scrollHeader);
+
+// modal viewwwwwwwwwwwwwwwwwwwwwwwwwwwww//////////////
+
+const modalViews = document.querySelectorAll(".form"),
+  modalBtns = document.querySelectorAll(".modal"),
+  modalCloses = document.querySelectorAll(".modal-close");
+
+let modal = function (modalClick) {
+  modalViews[modalClick].classList.add("active-modal");
+};
+
+modalBtns.forEach((modalBtn, i) => {
+  modalBtn.addEventListener("click", () => {
+    modal(i);
+  });
+});
+
+modalCloses.forEach((modalClose) => {
+  modalClose.addEventListener("click", () => {
+    modalViews.forEach((modalView) => {
+      modalView.classList.remove("active-modal");
+    });
+  });
+});
+
+// scroll appearrrrrrr
